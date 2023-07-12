5df1b66ee961421d593bddd444dbef2821a593c2
lizheming <i@imnerd.org>
2023-07-12 22:10:08 +0800

    fix: fix data store get error

--- src/store/deta.js
+++ src/store/deta.js
@@ -8,9 +8,14 @@ const drive = deta.Drive('douban');
 
 module.exports = {
   async get(filename) {
-    const data = await drive.get(path.join(STORE_PATH, filename));
-    const buffer = await data.arrayBuffer();
-    return Buffer.from(buffer, 'binary');
+    try {
+      const data = await drive.get(path.join(STORE_PATH, filename));
+      const buffer = await data.arrayBuffer();
+      return Buffer.from(buffer, 'binary');
+    } catch(e) {
+      console.log(e);
+      return;
+    }
   },
   async set(filename, data) {
     return drive.put(path.join(STORE_PATH, filename), { data });
